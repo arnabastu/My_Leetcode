2da6d7d2082b816f72cef5b8a8016ccbf71d702a
Arnab Chanda <arnabchanda475@gmail.com>
2026-07-06 20:17:10 +0530

    Time: 0 ms (100%), Space: 13.3 MB (35.24%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,14 +1,14 @@
 class Solution(object):
     def missingNumber(self, nums):
         n = len(nums)
-        freq = {}
-        for i in range(0 , n+1):
-            freq[i] = 0
+        n_sum = (n*(n+1))/2
+        nums_sum = 0
 
-        for num in nums:
-            freq[num] = 1
-            
-        for k,v in freq.items():
-            if v== 0:
-                return k
+        for i in range(0 , n):
+            nums_sum += nums[i]
+
+        missing_num = n_sum - nums_sum
+        return missing_num
+
+
 
