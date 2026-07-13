74c8ad9966fcd3021b652e6d44b482b4a0b6ee9c
Arnab Chanda <arnabchanda475@gmail.com>
2026-07-13 20:42:35 +0530

    Time: 7 ms (58.85%), Space: 20.2 MB (89.16%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -1,23 +1,16 @@
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
-        def mat_infi(matrix , row , col):
-            r = len(matrix)
-            c = len(matrix[0])
-
-            for i in range(0 , r):
-                if matrix[i][col] != 0:
-                    matrix[i][col] = float("inf")
-            for j in range(0 , c):
-                if matrix[row][j] != 0:
-                    matrix[row][j] = float("inf")
-        r = len(matrix) 
+        r = len(matrix)
         c = len(matrix[0])
+        rowtrack = [0 for _ in range(r)]
+        coltrack = [0 for _ in range(c)]
         for i in range(0 , r):
             for j in range(0 , c):
                 if matrix[i][j] == 0:
-                    mat_infi(matrix , i , j)
+                    rowtrack[i] = -1
+                    coltrack[j] = -1
 
         for i in range(0 , r):
             for j in range(0 , c):
-                if matrix[i][j] == float("inf"):
+                if rowtrack[i] == -1 or coltrack[j] ==-1:
                     matrix[i][j] = 0
